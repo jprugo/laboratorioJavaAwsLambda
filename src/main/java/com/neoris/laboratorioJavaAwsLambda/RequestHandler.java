9638367041a02c30407acf84265d04ac278c4dc3
Juan Rueda <juan.rueda@neoris.com>
2022-05-06 12:52:13 -0500

    Se actualiza repositorio

--- src/main/java/com/neoris/laboratorioJavaAwsLambda/RequestHandler.java
+++
@@ -1,6 +0,0 @@
-package com.neoris.laboratorioJavaAwsLambda;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class RequestHandler extends SpringBootRequestHandler<String,Object> {
-}
